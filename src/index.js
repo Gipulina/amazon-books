d21610ef4131f39c1861cbe08ef8b21566f3bcee
gipulina90 <georgi_palavecino@hotmail.com>
2023-04-22 14:56:49 -0300

    Using Map and Props in an array list

--- src/index.js
+++ src/index.js
@@ -1,40 +1,60 @@
-import React from 'react';
-import ReactDom from 'react-dom/client';
-import './index.css';
+import React from "react";
+import ReactDom from "react-dom/client";
+import "./index.css";
 
+const newBooks = [
+  {
+    title: "I Will Teach You to Be Rich",
+    author: "Ramit Sethi",
+    image:
+      "https://images-na.ssl-images-amazon.com/images/I/71zwHcw-D7L._AC_UL600_SR600,400_.jpg",
+    id: 1,
+  },
+  {
+    title: "The Last Thing He Told Me",
+    author: "Laura Dave",
+    image:
+      "https://images-na.ssl-images-amazon.com/images/I/71LyuJP7yUL._AC_UL600_SR600,400_.jpg",
+    id: 2,
+  },
+  {
+    title: "The Wager: A Tale of Shipwreck, Mutiny and Murder",
+    author: "David Grann ",
+    image:
+      "https://images-na.ssl-images-amazon.com/images/I/91qXDU9TXvL._AC_UL600_SR600,400_.jpg",
+    id: 3,
+  },
+];
 
-const BookList = () =>{
-    return ( 
+const BookList = () => {
+  return (
     <div>
       <h1>Amazon Book List</h1>
       <section className="booklist">
-        <Book />
-        <Book />
-        <Book />
-        <Book />
-        <Book />
-        <Book />
-        <Book />
-        <Book />
-        <Book />
+        {newBooks.map((book) => {
+          const { image, title, author, id } = book;
+          const key= {id};
+          return (
+            <div>
+              <Book book={book} key={id} />
+            </div>
+          );
+        })}
       </section>
     </div>
-    );
-}
-
-const Book = () =>{
-    return <article className='book'>
-        <Image/>
-        <Title/>
-        <Author/>
-    </article>
-}
-
-const Image = () =>  <img src="https://images-na.ssl-images-amazon.com/images/I/81Ct7OSVBDL._AC_UL600_SR600,400_.jpg" alt="You Can't Joke About That: Why Everything IsYou Can't Joke About That: Why Everything Is Funny, Nothing Is Sacred, and We're All in This Together"/>;
-const Title = () => <h3>You Can't Joke About That: Why Everything Is Funny, Nothing Is Sacred, and We're All in This Together</h3>;
-const Author = () => {
-    return <h4> Kat Timpf
-    </h4>;
+  );
 };
-const root = ReactDom.createRoot(document.getElementById('root'));
-root.render(<BookList/>);
+
+const Book = (props) => {
+  const { image, title, author } = props.book;
+return(
+  <article className="book">
+    <img src={image} alt={title}/>
+    <h3>{title}</h3>
+    <h4>{author}</h4>
+  </article>
+)
+};
+
+const root = ReactDom.createRoot(document.getElementById("root"));
+root.render(<BookList />);
